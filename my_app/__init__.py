eeed948f430df5abb1a730676ca57946cb3ef62d
william <william.live94@gmail.com>
2022-12-18 20:01:21 +0100

    added app_root variable

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,9 +2,9 @@ from pathlib import Path
 from .utils.logger import appLogger
 
 #logger can be used from anywhere in package
-APP_NAME = Path(__file__).parent.resolve().stem
-LOGGER = appLogger(APP_NAME).logger
+APP_ROOT = Path(__file__).parent.resolve()
+LOGGER = appLogger(APP_ROOT.stem).logger
 LOGGER.addHandler(appLogger.console_handler())
 
-RLOGGER = appLogger(f"R_{APP_NAME}").logger
+RLOGGER = appLogger(f"R_{APP_ROOT.stem}").logger
 RLOGGER.addHandler(appLogger.console_handler("\r"))
